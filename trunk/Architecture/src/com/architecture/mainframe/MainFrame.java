e3aea4df3c8e08be69123ad8506a8b7846f42b97
BaoBao.Lin.ioz@gmail.com <BaoBao.Lin.ioz@gmail.com@d2cd9691-9e17-7209-f43b-6ffb6704e259>
2015-01-17 21:04:28 +0000

    packages submit

---
+++ trunk/Architecture/src/com/architecture/mainframe/MainFrame.java
@@ -0,0 +1,12 @@
+package com.architecture.mainframe;
+
+import java.util.Scanner;
+
+public class MainFrame {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String instruction=sc.nextLine();
+	}
+
+}
