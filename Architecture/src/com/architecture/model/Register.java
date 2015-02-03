edcfd86db591124c67ec579c623e35e52f1c8ee1
BaoBao.Lin.ioz <BaoBao.Lin.ioz@gmail.com>
2015-02-03 08:19:56 +0000

    CPU, register, memory, Word以及simulator配置文件config, 修改MainFrame文件名-->Main

--- Architecture/src/com/architecture/model/Register.java
+++ Architecture/src/com/architecture/model/Register.java
@@ -2,4 +2,32 @@ package com.architecture.model;
 
 public class Register {
 
+	protected String name;
+	protected int[] data;
+	protected int size;
+	
+	public Register(String name, int[] data, int size){
+		this.name=name;
+		this.data=data;
+		this.size=size;
+		
+	}
+
+	public int[] getData() {
+		return data;
+	}
+
+	public void setData(int[] data) {
+		this.data = data;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	
 }
