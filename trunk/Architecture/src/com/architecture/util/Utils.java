33b87184b7ddfb9f044388cc729c3c58a8c0c43d
BaoBao.Lin.ioz@gmail.com <BaoBao.Lin.ioz@gmail.com@d2cd9691-9e17-7209-f43b-6ffb6704e259>
2015-02-03 05:16:00 +0000

    D->B

--- trunk/Architecture/src/com/architecture/util/Utils.java
+++ trunk/Architecture/src/com/architecture/util/Utils.java
@@ -6,10 +6,14 @@ package com.architecture.util;
  * A general methods collection.
  */
 public class Utils {
-
 	
-	public static String getBinaryFromDec(int val){
-		return null;
+	public static String getBinaryFromDec(int val, int size){
+		StringBuffer sb = new StringBuffer();
+		for(int i=0;i<size;i++){
+			sb.append( val%2);
+			val = val/2;
+		}
+		return sb.reverse().toString();
 	}
 	public static String getDecimalFromBin(int val){
 		return null;
