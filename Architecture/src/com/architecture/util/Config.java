cf0788d0c3c8943d2c802cd343b52a92603ca357
BaoBao.Lin.ioz <BaoBao.Lin.ioz@gmail.com>
2015-02-04 17:17:34 +0000

    更多寄存器配置信息加入

--- Architecture/src/com/architecture/util/Config.java
+++ Architecture/src/com/architecture/util/Config.java
@@ -8,8 +8,15 @@ package com.architecture.util;
 public class Config {
 
 	public static final int GPR_SIZE=16;
-	public static final int IR_SIZE=12;
+	public static final int IndexReg_SIZE=12;
 	public static final int WORD_SIZE=16;
 	public static int MEMORY_SIZE=2048;
+	public static final int PC_SIZE=12;
+	public static final int IR_SIZE=16;
+	public static final int MAR_SIZE=16;
+	public static final int MDR_SIZE=16;
+	public static final int CC_SIZE=4;
+	
+	public static final int ARR_SIZE=16;
 	
 }
