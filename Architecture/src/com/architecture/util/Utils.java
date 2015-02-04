451a638702aa53858f77fa9c105f87400726fb14
liuyihua1992 <liuyihua1992@gmail.com>
2015-02-04 05:16:11 +0000

    完善方法

--- Architecture/src/com/architecture/util/Utils.java
+++ Architecture/src/com/architecture/util/Utils.java
@@ -1,6 +1,7 @@
 package com.architecture.util;
 
 import com.architecture.app.Application;
+import com.architecture.model.InstructionSet;
 import com.architecture.model.Memory;
 
 /**
@@ -105,250 +106,101 @@ public class Utils {
 			}
 		}
 	}
+	
+	public static String decompose_3or4(int opcode, String instr){
+		StringBuffer code =new StringBuffer();
+		code.append(getBinaryFromDec(opcode, 6));
+		String instr_2=instr.substring(3);
+		// trim all leading and tailing whitespaces.fault-tolerant.
+		instr_2 = instr_2.trim();
+		
+		int index = instr_2.indexOf(',');
+		int bin = Integer.valueOf(instr_2.substring(0, index));
+		code.append(getBinaryFromDec(bin, 2)); // R
+		
+		instr_2 = instr_2.substring(index + 1);
+		index = instr_2.indexOf(',');
+		bin = Integer.valueOf(instr_2.substring(0, index));
+		code.append(getBinaryFromDec(bin, 2)); // IX
 
+		instr_2 = instr_2.substring(index + 1);
+		if (instr_2.indexOf(',') >= 0) {
+			code.append("0"); // I
+			index = instr_2.indexOf(',');
+			bin = Integer.valueOf(instr_2.substring(0, index));
+			code.append(getBinaryFromDec(bin, 5)); // Address
+		} else {
+			code.append("1"); // I
+			bin = Integer.valueOf(instr_2);
+			code.append(getBinaryFromDec(bin, 5));
+		}
+		return code.toString();
+	}
 	public static int getEffectiveAddressInDec(String instruction) {
 		return Integer.valueOf(getEffectiveAddress(instruction));
 	}
+	
+	public static String decompose_2(int opcode, String instr){
+		StringBuffer code=new StringBuffer();
+		String instr_2 = instr.substring(3);
+		instr_2 = instr_2.trim();// trim all leading and tailing
+									// whitespace.fault-tolerant.
 
+		int index = instr_2.indexOf(',');
+		int bin = Integer.valueOf(instr_2.substring(0, index));
+		code.append(getBinaryFromDec(bin, 2)); // R
+
+		code.append("000"); // IX and I
+
+		instr_2 = instr_2.substring(index + 1);
+		if (instr_2.indexOf(',') >= 0) {
+			code.replace(0, code.length(), "error!");
+		} else {
+			bin = Integer.valueOf(instr_2);
+			code.append(getBinaryFromDec(bin, 5));
+		}
+		return code.toString();
+	}
+	
 	public static String getCodeFromInstr(String instr) {
 		StringBuffer code = new StringBuffer();
 
 		try {
 			if (instr.toUpperCase().startsWith("LDR")) {
-				code.append(getBinaryFromDecInString(1, 6));// opcode
-				String instr_2 = instr.substring(3);
-				// trim all leading and tailing whitespaces.fault-tolerant.
-				instr_2 = instr_2.trim();
-
-				int index = instr_2.indexOf(',');
-				int bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2)); // R
-
-				instr_2 = instr_2.substring(index + 1);
-				index = instr_2.indexOf(',');
-				bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2)); // IX
-
-				instr_2 = instr_2.substring(index + 1);
-				if (instr_2.indexOf(',') >= 0) {
-					code.append("0"); // I
-					index = instr_2.indexOf(',');
-					bin = Integer.valueOf(instr_2.substring(0, index));
-					code.append(getBinaryFromDecInString(bin, 5)); // Address
-				} else {
-					code.append("1"); // I
-					bin = Integer.valueOf(instr_2);
-					code.append(getBinaryFromDecInString(bin, 5));
-				}
+				code.append(decompose_3or4((int)InstructionSet.instructionMap.get("LDR"),instr));
+				
 			}
 
 			else if (instr.toUpperCase().startsWith("STR")) {
-				code.append(getBinaryFromDecInString(2, 6));
-				String instr_2 = instr.substring(3);
-				// trim all leading and tailing whitespaces.fault-tolerant.
-				instr_2 = instr_2.trim();
-				int index = instr_2.indexOf(',');
-				int bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2));
-
-				instr_2 = instr_2.substring(index + 1);
-				index = instr_2.indexOf(',');
-				bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2));
-
-				instr_2 = instr_2.substring(index + 1);
-				if (instr_2.indexOf(',') >= 0) {
-					code.append("0");
-					index = instr_2.indexOf(',');
-					bin = Integer.valueOf(instr_2.substring(0, index));
-					code.append(getBinaryFromDecInString(bin, 5));
-				} else {
-					code.append("1"); // I
-					bin = Integer.valueOf(instr_2);
-					code.append(getBinaryFromDecInString(bin, 5));
-				}
+				code.append(decompose_3or4((int)InstructionSet.instructionMap.get("STR"),instr));
 			}
 
 			else if (instr.toUpperCase().startsWith("LDA")) {
-				code.append(getBinaryFromDecInString(3, 6));
-				String instr_2 = instr.substring(3);
-				instr_2 = instr_2.trim();// trim all leading and tailing
-											// whitespace.fault-tolerant.
-
-				int index = instr_2.indexOf(',');
-				int bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2));
-
-				instr_2 = instr_2.substring(index + 1);
-				index = instr_2.indexOf(',');
-				bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2));
-
-				instr_2 = instr_2.substring(index + 1);
-				if (instr_2.indexOf(',') >= 0) {
-					code.append("0");
-					index = instr_2.indexOf(',');
-					bin = Integer.valueOf(instr_2.substring(0, index));
-					code.append(getBinaryFromDecInString(bin, 5));
-				} else {
-					code.append("1"); // I
-					bin = Integer.valueOf(instr_2);
-					code.append(getBinaryFromDecInString(bin, 5));
-				}
+				code.append(decompose_3or4((int)InstructionSet.instructionMap.get("LDA"),instr));
 			}
 
 			else if (instr.toUpperCase().startsWith("LDX")) {
-				code.append(getBinaryFromDecInString(41, 6));
-				String instr_2 = instr.substring(3);
-				instr_2 = instr_2.trim();// trim all leading and tailing
-											// whitespace.fault-tolerant.
-
-				int index = instr_2.indexOf(',');
-				int bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2));
-
-				instr_2 = instr_2.substring(index + 1);
-				index = instr_2.indexOf(',');
-				bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2));
-
-				instr_2 = instr_2.substring(index + 1);
-				if (instr_2.indexOf(',') >= 0) {
-					code.append("0");
-					index = instr_2.indexOf(',');
-					bin = Integer.valueOf(instr_2.substring(0, index));
-					code.append(getBinaryFromDecInString(bin, 5));
-				} else {
-					code.append("1"); // I
-					bin = Integer.valueOf(instr_2);
-					code.append(getBinaryFromDecInString(bin, 5));
-				}
+				code.append(decompose_3or4((int)InstructionSet.instructionMap.get("LDX"),instr));
 			}
 
 			else if (instr.toUpperCase().startsWith("STX")) {
-				code.append(getBinaryFromDecInString(42, 6));
-				String instr_2 = instr.substring(3);
-				instr_2 = instr_2.trim();// trim all leading and tailing
-											// whitespace.fault-tolerant.
-
-				int index = instr_2.indexOf(',');
-				int bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2));
-
-				instr_2 = instr_2.substring(index + 1);
-				index = instr_2.indexOf(',');
-				bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2));
-
-				instr_2 = instr_2.substring(index + 1);
-				if (instr_2.indexOf(',') >= 0) {
-					code.append("0");
-					index = instr_2.indexOf(',');
-					bin = Integer.valueOf(instr_2.substring(0, index));
-					code.append(getBinaryFromDecInString(bin, 5));
-				} else {
-					code.append("1"); // I
-					bin = Integer.valueOf(instr_2);
-					code.append(getBinaryFromDecInString(bin, 5));
-				}
+				code.append(decompose_3or4((int)InstructionSet.instructionMap.get("STX"),instr));
 			}
 
 			else if (instr.toUpperCase().startsWith("AMR")) {
-				code.append(getBinaryFromDecInString(4, 6));// opcode
-				String instr_2 = instr.substring(3);
-				instr_2 = instr_2.trim();// trim all leading and tailing
-											// whitespace.fault-tolerant.
-
-				int index = instr_2.indexOf(',');
-				int bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2)); // R
-
-				instr_2 = instr_2.substring(index + 1);
-				index = instr_2.indexOf(',');
-				bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2)); // IX
-
-				instr_2 = instr_2.substring(index + 1);
-				if (instr_2.indexOf(',') >= 0) {
-					code.append("0"); // I
-					index = instr_2.indexOf(',');
-					bin = Integer.valueOf(instr_2.substring(0, index));
-					code.append(getBinaryFromDecInString(bin, 5)); // Address
-				} else {
-					code.append("1"); // I
-					bin = Integer.valueOf(instr_2);
-					code.append(getBinaryFromDecInString(bin, 5));
-				}
+				code.append(decompose_3or4((int)InstructionSet.instructionMap.get("AMR"),instr));// opcode
 			}
 
 			else if (instr.toUpperCase().startsWith("SMR")) {
-				code.append(getBinaryFromDecInString(5, 6));// opcode
-				String instr_2 = instr.substring(3);
-				instr_2 = instr_2.trim();// trim all leading and tailing
-											// whitespace.fault-tolerant.
-
-				int index = instr_2.indexOf(',');
-				int bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2)); // R
-
-				instr_2 = instr_2.substring(index + 1);
-				index = instr_2.indexOf(',');
-				bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2)); // IX
-
-				instr_2 = instr_2.substring(index + 1);
-				if (instr_2.indexOf(',') >= 0) {
-					code.append("0"); // I
-					index = instr_2.indexOf(',');
-					bin = Integer.valueOf(instr_2.substring(0, index));
-					code.append(getBinaryFromDecInString(bin, 5)); // Address
-				} else {
-					code.append("1"); // I
-					bin = Integer.valueOf(instr_2);
-					code.append(getBinaryFromDecInString(bin, 5));
-				}
+				code.append(decompose_3or4((int)InstructionSet.instructionMap.get("SMR"),instr));// opcode
 			}
 
 			else if (instr.toUpperCase().startsWith("AIR")) {
-				code.append(getBinaryFromDecInString(6, 6));// opcode
-				String instr_2 = instr.substring(3);
-				instr_2 = instr_2.trim();// trim all leading and tailing
-											// whitespace.fault-tolerant.
-
-				int index = instr_2.indexOf(',');
-				int bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2)); // R
-
-				code.append("000"); // IX and I
-
-				instr_2 = instr_2.substring(index + 1);
-				if (instr_2.indexOf(',') >= 0) {
-					code.replace(0, code.length() - 1, "error!");
-				} else {
-					bin = Integer.valueOf(instr_2);
-					code.append(getBinaryFromDecInString(bin, 5));
-				}
+				code.append(decompose_2((int)InstructionSet.instructionMap.get("AIR"),instr));// opcode
 			}
 
 			else if (instr.toUpperCase().startsWith("SIR")) {
-				code.append(getBinaryFromDecInString(6, 6));// opcode
-				String instr_2 = instr.substring(3);
-				instr_2 = instr_2.trim();// trim all leading and tailing
-											// whitespace.fault-tolerant.
-
-				int index = instr_2.indexOf(',');
-				int bin = Integer.valueOf(instr_2.substring(0, index));
-				code.append(getBinaryFromDecInString(bin, 2)); // R
-
-				code.append("000"); // IX and I
-
-				instr_2 = instr_2.substring(index + 1);
-				if (instr_2.indexOf(',') >= 0) {
-					code.replace(0, code.length(), "error!");
-				} else {
-					bin = Integer.valueOf(instr_2);
-					code.append(getBinaryFromDecInString(bin, 5));
-				}
+				code.append(decompose_2((int)InstructionSet.instructionMap.get("SIR"),instr));// opcode
 			}
 		} catch (Exception e) {
 			// e.printStackTrace();
