9dc680b17174082b7c62b564cd71647ed31b1f58
BaoBao.Lin.ioz@gmail.com <BaoBao.Lin.ioz@gmail.com@d2cd9691-9e17-7209-f43b-6ffb6704e259>
2015-02-04 17:15:41 +0000

    ProgramCounter

---
+++ trunk/Architecture/src/com/architecture/model/ProgramCounter.java
@@ -0,0 +1,12 @@
+package com.architecture.model;
+
+import com.architecture.util.Config;
+
+public class ProgramCounter extends Register {
+
+	public ProgramCounter(String name, int[] data, int size) {
+		super(name, data, Config.PC_SIZE);
+		// TODO Auto-generated constructor stub
+	}
+
+}
