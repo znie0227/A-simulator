d355e2ab4676ed4c5184fb498194f7fee7a1eb3b
BaoBao.Lin.ioz <BaoBao.Lin.ioz@gmail.com>
2015-02-04 00:20:42 +0000

    用十进制返回register的数据

--- Architecture/src/com/architecture/model/Register.java
+++ Architecture/src/com/architecture/model/Register.java
@@ -1,28 +1,40 @@
 package com.architecture.model;
 
+import com.architecture.util.Utils;
+
 public class Register {
 
 	protected String name;
 	protected int[] data;
 	protected int size;
-	
-	public Register(String name, int[] data, int size){
-		this.name=name;
-		this.data=data;
-		this.size=size;
-		
+
+	public Register(String name, int[] data, int size) {
+		this.name = name;
+		this.data = data;
+		this.size = size;
+
 	}
 
 	public int[] getData() {
 		return data;
 	}
+
 	public String getDataInString() {
 		StringBuffer sb = new StringBuffer();
-		for (int i=0;i<size;i++)
-		{
+		for (int i = 0; i < size; i++) {
 			sb.append(data[i]);
 		}
 		return sb.toString();
+	}
+	
+	
+	/**
+	  
+	 * @return data in decimal-format
+	 */
+	public int getDecData(){
+		return Integer.valueOf(Utils.getDecimalFromBin(getData()));
+		
 	}
 
 	public void setData(int[] data) {
@@ -36,6 +48,5 @@ public class Register {
 	public void setSize(int size) {
 		this.size = size;
 	}
-	
-	
+
 }
