8ab481d5ee2ff6e16a598a5c738141077da8c6e3
BaoBao.Lin.ioz <BaoBao.Lin.ioz@gmail.com>
2015-02-05 04:03:06 +0000

    log to console panel

--- Architecture/src/com/architecture/model/Log.java
+++ Architecture/src/com/architecture/model/Log.java
@@ -1,9 +1,12 @@
 package com.architecture.model;
 
+import com.architecture.view.ConsolePanel;
+
 public class Log {
 
 	public static void d(String msg){
 		// TODO to be improved
 		System.out.println(msg);
+		ConsolePanel.writeToConsole(msg);
 	}
 }
