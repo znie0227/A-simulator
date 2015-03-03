2796854c0f2b21b7092e70e4085d4e021d2e07f7
BaoBao.Lin.ioz@gmail.com <BaoBao.Lin.ioz@gmail.com@d2cd9691-9e17-7209-f43b-6ffb6704e259>
2015-03-03 01:04:18 +0000

    程序起始位置加入

--- trunk/Architecture/src/com/architecture/util/Constants.java
+++ trunk/Architecture/src/com/architecture/util/Constants.java
@@ -12,4 +12,6 @@ public class Constants {
 	public static final int CPU_STATE_IDLE=1000;
 	public static final int CPU_STATE_RUNNING=1001;
 	public static final int CPU_STATE_FINISHED=1002;
+	
+	public static final int OCTAL_10=8;
 }
