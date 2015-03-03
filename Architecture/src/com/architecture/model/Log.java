c064fd591ae95de600c53dd88e8dd5ac7e28fd92
BaoBao.Lin.ioz <BaoBao.Lin.ioz@gmail.com>
2015-03-03 01:06:55 +0000

    log新功能加入

--- Architecture/src/com/architecture/model/Log.java
+++ Architecture/src/com/architecture/model/Log.java
@@ -1,12 +1,25 @@
 package com.architecture.model;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import com.architecture.view.ConsolePanel;
 
 public class Log {
+	
+	private static Map logMap = new LinkedHashMap<String, String>();
 
 	public static void d(String msg){
 		// TODO to be improved
 		System.out.println(msg);
 		ConsolePanel.writeToConsole(msg);
+		logMap.put("Debug", msg);
+	}
+	
+	public static void e(String msg){
+		// TODO to be improved
+		System.out.println(msg);
+		ConsolePanel.writeToConsole(msg);
+		logMap.put("Error", msg);
 	}
 }
