757559a49ce0927201729c36508443ddf8bc4ddc
BaoBao.Lin.ioz <BaoBao.Lin.ioz@gmail.com>
2015-03-04 06:57:55 +0000

    reset功能加入（register，memory，IO devices）

--- Architecture/src/com/architecture/app/Application.java
+++ Architecture/src/com/architecture/app/Application.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import com.architecture.device.CardReader;
+import com.architecture.device.Device;
 import com.architecture.model.Memory;
 import com.architecture.model.Register;
 import com.architecture.util.Config;
@@ -23,11 +25,10 @@ public class Application {
 		// Scanner sc = new Scanner(System.in);
 		// String instruction=sc.nextLine();
 
-		
 		init();
-		
+
 		MainFrame mv = new MainFrame();
-		
+
 	}
 
 	private static void init() {
@@ -45,46 +46,52 @@ public class Application {
 		addRegister("X1", Config.IndexReg_SIZE);
 		addRegister("X2", Config.IndexReg_SIZE);
 		addRegister("X3", Config.IndexReg_SIZE);
-		
+
 		addRegister("PC", Config.PC_SIZE);
 		addRegister("CC", Config.CC_SIZE);
 		addRegister("IR", Config.IR_SIZE);
 		addRegister("MAR", Config.MAR_SIZE);
 		addRegister("MDR", Config.MDR_SIZE);
 		addRegister("Carry", Config.MDR_SIZE);
-		
+
 		addRegister("ARR", Config.ARR_SIZE);
 		addRegister("MRR", Config.MRR_SIZE);
 		addRegister("SRR", Config.SRR_SIZE);
-//		addRegister("CRR", Config.MDR_SIZE);
+		// addRegister("CRR", Config.MDR_SIZE);
 		addRegister("LRR", Config.LRR_SIZE);
-		
 
 	}
 
-	private static void addRegister(String name, int size){
+	private static void addRegister(String name, int size) {
 		registerMap.put(name, new Register(name, new int[size], size));
 		registerList.add(name);
 	}
-	
-	public static Register getRegisterByName(String name){
+
+	public static Register getRegisterByName(String name) {
 		return registerMap.get(name);
 	}
-	
-	public static void reset(){
-		for (String name:registerList) {
+
+	public static void reset() {
+		for (String name : registerList) {
 			resetRegisters(name);
 		}
+		resetMemory();
+		resetDevices();
 	}
-	
-	public static void resetRegisters(String name){
+
+	public static void resetRegisters(String name) {
 		getRegisterByName(name).setDataByDec(0);
 	}
-	
-	public static void resetMemory(){
-		for (int i=0;i<Config.MEMORY_SIZE;i++) {
+
+	public static void resetMemory() {
+		for (int i = 0; i < Config.MEMORY_SIZE; i++) {
 			Memory.getInstance().write(i, new int[Config.MEMORY_SIZE]);
 		}
 	}
-	
+
+	public static void resetDevices(){
+		for (int i=0;i<3;i++){
+			Device.getDevice(i).reset();
+		}
+	}
 }
