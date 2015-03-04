6286f07ba51c40854941a4e060f7f34afcf98ad1
BaoBao.Lin.ioz <BaoBao.Lin.ioz@gmail.com>
2015-03-04 07:00:15 +0000

    IO各功能加入，printer，keyboard，etc.

--- Architecture/src/com/architecture/view/IOSystemPrinterPanel.java
+++ Architecture/src/com/architecture/view/IOSystemPrinterPanel.java
@@ -3,110 +3,162 @@ package com.architecture.view;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.TextArea;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class IOSystemPrinterPanel extends JPanel{
-	
+import com.architecture.app.CPU;
+import com.architecture.device.ConsoleKeyboard;
+import com.architecture.thread.InputFromDeviceThread;
+
+public class IOSystemPrinterPanel extends JPanel {
+
 	private static final long serialVersionUID = -3819691367521335747L;
-	
+
 	private boolean flashing = false;
 	private int degree = 0;
 	private int increase = 1;
 	private int MAX = 20;
-	
-	private static IOSystemPrinterPanel ioSystemPrinterPanel;
-	
-	private Thread runningThread;
-	
-	private JTextArea outputField = new JTextArea();
 
-	public IOSystemPrinterPanel()
-	{
-		
+	private static IOSystemPrinterPanel ioSystemPrinterPanel;
+
+	private Thread runningThread;
+
+	private static JTextArea outputField = new JTextArea();
+
+	private JCheckBox linkToKeyboard = new JCheckBox("Link To Keyboard");
+	
+	public static boolean input=false;
+
+	public IOSystemPrinterPanel() {
+		ioSystemPrinterPanel=this;
 		this.setLayout(null);
 		AbstractButton iosystemprinterPanel;
-//		this.setBounds(608,0,210,250);		
-		this.setBorder(BorderFactory.createTitledBorder("I/O SystemPrinter "));
-		
+		// this.setBounds(608,0,210,250);
+		this.setBorder(BorderFactory.createTitledBorder("I/O SystemPrinter"));
+
 		outputField.setLayout(null);
-//		outputField.setBounds(620,20,200,100);		
+		// outputField.setBounds(620,20,200,100);
 		outputField.setVisible(true);
-		outputField.setSize(180, 200);
-		outputField.setLocation(10, 32);
+		outputField.setSize(180, 150);
+		outputField.setLocation(10, 82);
 		outputField.setLineWrap(true);
+		outputField.setEditable(false);
+		outputField.setBackground(Color.BLACK);
+		outputField.setForeground(Color.WHITE);
+		linkToKeyboard.setVisible(true);
+		linkToKeyboard.setLocation(10, 50);
+		linkToKeyboard.setSize(180, 20);
 		this.add(outputField);
+		this.add(linkToKeyboard);
 		this.setVisible(true);
+
+		linkToKeyboard.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				int value = (int) e.getKeyChar();
+				if (value > 0 && value < 128) {
+					System.out.println((char)value);
+					input=true;
+					InputFromDeviceThread.setKeyEvent(value);
+					CPU.keyEvent=value;
+					synchronized (CPU.currentThread) {
+					
+					CPU.currentThread.notify();
+					}
+				}
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
 	}
-	
-	public void startFlash(){
+
+	public void startFlash() {
 		flashing = true;
-		
-		if(runningThread==null||!runningThread.isAlive()){
-			runningThread = new Thread(){
+
+		if (runningThread == null || !runningThread.isAlive()) {
+			runningThread = new Thread() {
 				@Override
-				public void run(){
-					while(flashing){
-						repaint();
+				public void run() {
+					while (flashing) {
+//						IOSystemPrinterPanel.this.repaint();
 						try {
 							Thread.sleep(50);
-							degree+=increase;
-							if(degree>=MAX)
+							degree += increase;
+							if (degree >= MAX)
 								increase = -1;
-							if(degree<=0)
+							if (degree <= 0)
 								increase = 1;
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
+						paintComponent(IOSystemPrinterPanel.getInstance().linkToKeyboard.getGraphics());
 					}
 					runningThread = null;
+//					ioSystemPrinterPanel.linkToKeyboard.setBackground(
+//							new Color(20 + 8 * degree, 150 + 5 * degree,
+//							20 + 5 * degree)
+//							);
 				}
 			};
 			runningThread.start();
 		}
 	}
-	
-	public void stopFlash(){
+
+	public void stopFlash() {
 		flashing = false;
 		repaint();
 	}
-	
-	
-	
 
 	@Override
 	protected void paintComponent(Graphics g) {
-		
-		System.out.println("paint");
-		//Dimension size = this.getSize();
-		if(flashing){
-			g.setColor(new Color(20+8*degree,150+5*degree,20+5*degree));
-			g.fillRoundRect(5, 17, 200, 175, 4, 4);
-			g.setColor(Color.gray);
-			g.drawRoundRect(5, 17, 200, 175, 6, 6);
+
+		g=this.linkToKeyboard.getGraphics();
+		// Dimension size = this.getSize();
+		if (flashing) {
+			g.setColor(new Color(20 + 8 * degree, 150 + 5 * degree,
+					20 + 5 * degree));
+//			g.fillRoundRect(5, 17, 200, 175, 4, 4);
+//			g.setColor(Color.gray);
+//			g.drawRoundRect(5, 17, 200, 175, 6, 6);
 		}
-		
-		super.paintComponent(g);
+
+//		super.paintComponent(g);
 		this.paintComponents(g);
-		
-	}
-	
-	@Override
-	public void updateUI() {
-		// TODO Auto-generated method stub
-		System.out.println("updateui");
-		super.updateUI();
 	}
 
-	public static IOSystemPrinterPanel getInstance(){
-		if (ioSystemPrinterPanel==null) {
-			ioSystemPrinterPanel=new IOSystemPrinterPanel();
+
+	public static IOSystemPrinterPanel getInstance() {
+		if (ioSystemPrinterPanel == null) {
+			ioSystemPrinterPanel = new IOSystemPrinterPanel();
 		}
 		return ioSystemPrinterPanel;
+	}
+	
+	public JCheckBox getLinkToKeyboardBox(){
+		return this.linkToKeyboard; 
+	}
+	
+	public static void writeToPrinter(String msg){
+		outputField.setText(outputField.getText()+msg);
 	}
 
 }
