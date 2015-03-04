4ccee692ffa306f6ad20697c9977d1a6c5620337
BaoBao.Lin.ioz@gmail.com <BaoBao.Lin.ioz@gmail.com@d2cd9691-9e17-7209-f43b-6ffb6704e259>
2015-03-04 06:58:42 +0000

    devices

---
+++ trunk/Architecture/src/com/architecture/device/CardReader.java
@@ -0,0 +1,30 @@
+package com.architecture.device;
+
+public class CardReader extends Device {
+
+	private static StringBuilder sb = new StringBuilder();
+
+	private int status = Device.STATUS_IDLE;
+
+	@Override
+	public void write(String msg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int read() {
+		return sb.charAt(sb.length() - 1);
+	}
+
+	@Override
+	public void reset() {
+		sb = new StringBuilder();
+	}
+
+	@Override
+	public int getDeviceStatus() {
+		return status;
+	}
+
+}
